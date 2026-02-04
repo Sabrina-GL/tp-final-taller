f26f7a574da149193776a5f8c040444180f40bb4
stephany <galindostephany@gmail.com>
2026-02-04 00:54:46 -0500

    Modifiqué chat_room.ex y chat_manager.ex para la feauture de  búsqueda de mensajes por palabra clave dentro de una conversación

--- chat_app/lib/chat_app/chat_manager.ex
+++ chat_app/lib/chat_app/chat_manager.ex
@@ -1,12 +1,46 @@
 defmodule ChatApp.ChatManager do
   use GenServer
 
-  # Client API
+  # CLIENT API
+
   def start_link(_) do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
+  def create_chat(chat_id, participants) do
+    GenServer.call(__MODULE__, {:create_chat, chat_id, participants})
+  end
+
+  def search_messages(chat_id, keyword) do
+    GenServer.call(__MODULE__, {:search_messages, chat_id, keyword})
+  end
+
+  # SERVER
+
   def init(state) do
     {:ok, state}
+  end
+
+  def handle_call({:create_chat, chat_id, participants}, _from, state) do
+    {:ok, pid} =
+      ChatApp.ChatRoom.start_link(%{
+        id: chat_id,
+        participants: participants,
+        messages: []
+      })
+
+    new_state = Map.put(state, chat_id, pid)
+    {:reply, {:ok, pid}, new_state}
+  end
+
+  def handle_call({:search_messages, chat_id, keyword}, _from, state) do
+    case Map.get(state, chat_id) do
+      nil ->
+        {:reply, {:error, :chat_not_found}, state}
+
+      pid ->
+        results = ChatApp.ChatRoom.search_messages(pid, keyword)
+        {:reply, {:ok, results}, state}
+    end
   end
 end
--- chat_app/lib/chat_app/chat_room.ex
+++ chat_app/lib/chat_app/chat_room.ex
@@ -1,12 +1,51 @@
 defmodule ChatApp.ChatRoom do
   use GenServer
 
-  # Client API
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts)
+  # CLIENT API
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state)
   end
+
+  def add_message(pid, from, text) do
+    GenServer.cast(pid, {:add_message, from, text})
+  end
+
+  def search_messages(pid, keyword) do
+    GenServer.call(pid, {:search_messages, keyword})
+  end
+
+  # SERVER
 
   def init(state) do
     {:ok, state}
+  end
+
+  def handle_cast({:add_message, from, text}, state) do
+    message = %{
+      from: from,
+      text: text,
+      timestamp: DateTime.utc_now()
+    }
+
+    messages =
+      [message | state.messages]
+      |> Enum.take(10)
+
+    new_state = %{state | messages: messages}
+    {:noreply, new_state}
+  end
+
+  def handle_call({:search_messages, keyword}, _from, state) do
+    results =
+      state.messages
+      |> Enum.filter(fn msg ->
+        String.contains?(
+          String.downcase(msg.text),
+          String.downcase(keyword)
+        )
+      end)
+
+    {:reply, results, state}
   end
 end
